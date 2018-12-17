2daa0b3eaa06456cac57e9b06261baa0ca848c84
David Vedvick <dvedvick@gmail.com>
2018-12-16 18:05:51 -0600

    Expect stack overflows to handle

---
+++ src/bin/test-exception-stack-overflow.rs
@@ -0,0 +1,40 @@
+#![no_std]
+#![cfg_attr(not(test), no_main)]
+#![cfg_attr(test, allow(dead_code, unused_macros, unused_imports))]
+
+use blog_os::{exit_qemu, serial_println};
+use core::panic::PanicInfo;
+
+#[cfg(not(test))]
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    blog_os::interrupts::init_idt();
+
+    fn stack_overflow() {
+        stack_overflow();
+    }
+
+    stack_overflow();
+
+    serial_println!("failed");
+    serial_println!("Expected to panic!");
+
+    unsafe {
+        exit_qemu();
+    }
+
+    loop {}
+}
+
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("ok");
+
+    unsafe {
+        exit_qemu();
+    }
+
+    loop {}
+}
